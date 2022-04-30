961f4287ca4d5e9917e8bf4155ec22561be5dfbf
Edarcega <edimar.eds@gmail.com>
2022-04-30 13:14:04 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -26,15 +35,15 @@ public class MainViewController implements Initializable {
 	public void onMenuItemVendedorAction() {
 		System.out.println("onMenuItemVendedorAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartamentoAction() {
 		System.out.println("onMenuItemDepartamentoAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemSobreAction() {
-		System.out.println("onMenuItemDepartamentoAction");
+		loadView("/gui/Sobre.fxml");
 	}
 
 	/*
@@ -52,6 +61,29 @@ public class MainViewController implements Initializable {
 	 */
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
+	}
+
+	// Função para abrir outra tela
+	// o synchronized garante que o processo não será interrompido
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			Scene mainScene = Main.getMainScene(); // pega a cena principal da classe main
+			// O metodo getRoot pega o primeiro elemento da view
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); // Pega a referência para o Vbox que
+																					// está na janela principal
+			
+			Node mainMenu = mainVbox.getChildren().get(0); // Primeiro filho do Vbox da janela principal o Main Menu.
+			mainVbox.getChildren().clear(); // Limpa todos os fihos do main Vbox
+			mainVbox.getChildren().add(mainMenu); // Adiciona o main menu
+			mainVbox.getChildren().addAll(newVBox.getChildren()); // Adiciona os filhos do new VOX (a janela que estiver abrindo que veio na referencia)
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro loading view", e.getMessage(), AlertType.ERROR);
+		} catch (IllegalStateException e) {
+			Alerts.showAlert("IO Exception", "Erro loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
