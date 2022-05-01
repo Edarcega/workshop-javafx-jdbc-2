d66549088b95e746dbe2e0f91e1153bd61bca04a
Edarcega <edimar.eds@gmail.com>
2022-05-01 12:55:43 -0300

    SellerList

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class MainViewController implements Initializable {
 
@@ -34,8 +35,11 @@ public class MainViewController implements Initializable {
 	// Metodos para tratar ações de menu
 
 	@FXML
-	public void onMenuItemVendedorAction() {
-		System.out.println("onMenuItemVendedorAction");
+	public void onMenuItemSellerAction() {
+		loadView("/gui/SellerList.fxml", (SellerListController controller) -> {
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});
 	}
 
 	@FXML
---
+++ src/gui/SellerListController.java
@@ -0,0 +1,187 @@
+package gui;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import application.Main;
+import db.DbIntegrityException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Seller;
+import model.services.SellerService;
+
+public class SellerListController implements Initializable, DataChangeListener {
+
+	// dependencia para o serviço que busca os departamentos
+	private SellerService service;
+
+	// referencias para os componentes da tela
+
+	@FXML
+	private TableView<Seller> tableViewSeller;
+
+	@FXML
+	private TableColumn<Seller, Integer> tableColumnId; // Tipo da entidade e tipo da coluna
+
+	@FXML
+	private TableColumn<Seller, String> tableColumnNome;
+
+	@FXML
+	TableColumn<Seller, Seller> tableColumnEDIT;
+
+	@FXML
+	TableColumn<Seller, Seller> tableColumnREMOVE;
+
+	@FXML
+	private Button btNew;
+
+	private ObservableList<Seller> obslist;
+
+	@FXML
+	private void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);// pegar refenencia para o Stage atual
+		Seller obj = new Seller();
+		createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);// para para criar a janela
+	}
+
+	// principio solid de inversão de controle
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	// Metodo para iniciar componentes na tela
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnNome.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+		// macete para a tablema acompanhar a janela
+
+		Stage stage = (Stage) Main.getMainScene().getWindow(); // pegar a janela
+		tableViewSeller.prefHeightProperty().bind(stage.heightProperty()); // bind = ligar
+	}
+
+	// Metodo responsavel por acessar o serviço
+	// Carregar os departamentos e enviar para observablelist
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+
+		List<Seller> list = service.fidAll();
+		obslist = FXCollections.observableArrayList(list); // Instancia o observable list pegando os dados da lista
+		tableViewSeller.setItems(obslist);
+		initEditButtons();
+		initRemoveButtons();
+	}
+
+
+	// Janela de diálogo (modal)
+	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
+//		// logica para abrir a janela de formulário
+//		try {
+//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));// Pegar a view
+//			Pane pane = loader.load();// Carregar a view
+//
+//			SellerFormController controller = loader.getController();// Pegar a referência do controller do
+//																			// formulario
+//			controller.setSeller(obj);
+//			controller.setSellerService(new SellerService());
+//			controller.subscribeDataChangeListener(this);// Inscreve o objeto para receber evento
+//			controller.updateFormData();
+//
+//			Stage dialogStage = new Stage();
+//			dialogStage.setTitle("Entrada de dados do departamento");
+//			dialogStage.setScene(new Scene(pane));
+//			dialogStage.setResizable(false); // Trava dimensionamento da janela
+//			dialogStage.initOwner(parentStage); // Stage pai da janela
+//			dialogStage.initModality(Modality.WINDOW_MODAL);
+//			dialogStage.showAndWait();
+//		} catch (IOException e) {
+//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+//		}
+	}
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();
+	}
+
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Seller, Seller>() {
+			private final Button button = new Button("edit");
+
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
+			}
+		});
+	}
+
+	private void initRemoveButtons() {
+		tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnREMOVE.setCellFactory(param -> new TableCell<Seller, Seller>() {
+			private final Button button = new Button("remove");
+
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(event -> removeEntity(obj));
+			}
+		});
+	}
+
+	private void removeEntity(Seller obj) {
+		// optional é um objto que carrega outro objeto dentro dele, podendo estar presente ou não
+		Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete?");
+		
+		if (result.get() == ButtonType.OK) {
+			if (service == null) {
+				throw new IllegalStateException("Service was null");
+			}
+			try {
+				service.remove(obj);
+				updateTableView();
+			}
+			catch (DbIntegrityException e) {
+				Alerts.showAlert("Error removing object", null, e.getMessage(), AlertType.ERROR);
+			}
+		}
+	
+	}
+
+}
